98bb2761ab6014c54d9ce725e0afcd9204c1784d
MaybeLaterx <MaybeLater_x@hotmail.co.uk>
2025-12-26 15:18:29 +0000

    final brushing up

--- src/main/java/spireQuests/quests/maybelaterx/CalculatedKillerQuest.java
+++ src/main/java/spireQuests/quests/maybelaterx/CalculatedKillerQuest.java
@@ -8,7 +8,7 @@ import spireQuests.quests.maybelaterx.relics.BalancingStonesRelic;
 public class CalculatedKillerQuest extends AbstractQuest {
 
     public CalculatedKillerQuest() {
-        super(QuestType.SHORT, QuestDifficulty.NORMAL);
+        super(QuestType.SHORT, QuestDifficulty.EASY);
         new TriggerTracker<>(QuestTriggers.EXACT_KILL, 3)
                 .add(this);
         addReward(new QuestReward.RelicReward(new BalancingStonesRelic()));
